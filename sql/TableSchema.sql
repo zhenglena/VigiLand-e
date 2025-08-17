4edf95ff8074704088603b9ea1bc93ef57405017
zhenglena <zhenglena@gmail.com>
2025-08-16 21:15:57 -0500

    Add sql script

---
+++ sql/TableSchema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Violations(
+	id VARCHAR(8) PRIMARY KEY,
+  	violation_date DATE NOT NULL,
+  	violation_code VARCHAR(8) NOT NULL,
+  	violation_status VARCHAR(10) NOT NULL,
+  	violation_description TEXT,
+  	violation_inspector_comments TEXT,
+  	address VARCHAR(100) NOT NULL
+);
+
+CREATE INDEX violations_address_index ON Violations(address);
+
+CREATE TABLE Scofflaws(
+	record_id VARCHAR(30) PRIMARY KEY,
+  	address VARCHAR(100) NOT NULL,
+  	building_list_date DATE NOT NULL
+);
+
+CREATE INDEX scofflaws_address_index ON Scofflaws(address);
+CREATE INDEX scofflaws_list_date_index ON Scofflaws(building_list_date);
+
+CREATE TABLE Comments (
+  	comment_id SERIAL PRIMARY KEY,
+	author VARCHAR(15),
+  	date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  	address VARCHAR(100) NOT NULL,
+  	comment TEXT
+);
