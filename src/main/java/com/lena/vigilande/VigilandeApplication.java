6aee658b50979396bdb24a78a184b86aebd1fb1d
zhenglena <zhenglena@gmail.com>
2025-08-16 21:15:09 -0500

    initial commit

---
+++ src/main/java/com/lena/vigilande/VigilandeApplication.java
@@ -0,0 +1,13 @@
+package com.lena.vigilande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigilandeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lena/vigilande/VigilandeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lena.vigilande;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
