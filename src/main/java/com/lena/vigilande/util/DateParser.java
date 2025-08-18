e518acf5bb361372e8cd1a0c7a18b4358cdc289d
zhenglena <zhenglena@gmail.com>
2025-08-17 21:11:34 -0500

    Add DateParser

---
+++ src/main/java/com/lena/vigilande/util/DateParser.java
@@ -0,0 +1,15 @@
+package com.lena.vigilande.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateParser {
+
+    public static LocalDate parseDate(String date) throws DateTimeParseException {
+
+        LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("MM/dd/uuuu"));
+
+        return localDate;
+    }
+}
---
+++ src/test/java/com/lena/vigilande/parser/DateParserTest.java
@@ -0,0 +1,22 @@
+package com.lena.vigilande.parser;
+
+import com.lena.vigilande.util.DateParser;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class DateParserTest {
+
+    @Test
+    public void dateParser_successful() {
+        String input = "08/08/2000";
+        LocalDate actualDate = DateParser.parseDate(input);
+
+        assertEquals(LocalDate.of(2000,Month.AUGUST,8), actualDate);
+    }
+}
