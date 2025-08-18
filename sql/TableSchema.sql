c3a52970d2405737601f77d8def4ad4a4962d4d1
zhenglena <zhenglena@gmail.com>
2025-08-17 21:54:26 -0500

    Modify table schema columns

--- sql/TableSchema.sql
+++ sql/TableSchema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Violations(
 	id VARCHAR(8) PRIMARY KEY,
-  	violation_date DATE NOT NULL,
-  	violation_code VARCHAR(8) NOT NULL,
-  	violation_status VARCHAR(10) NOT NULL,
+  	violation_date DATE,
+  	violation_code VARCHAR(8),
+  	violation_status VARCHAR(10),
   	violation_description TEXT,
   	violation_inspector_comments TEXT,
-  	address VARCHAR(100) NOT NULL
+  	address VARCHAR(100)
 );
 
 CREATE INDEX violations_address_index ON Violations(address);
 
 CREATE TABLE Scofflaws(
 	record_id VARCHAR(30) PRIMARY KEY,
-  	address VARCHAR(100) NOT NULL,
-  	building_list_date DATE NOT NULL
+  	address VARCHAR(100),
+  	building_list_date DATE
 );
 
 CREATE INDEX scofflaws_address_index ON Scofflaws(address);
@@ -22,7 +22,7 @@ CREATE INDEX scofflaws_list_date_index ON Scofflaws(building_list_date);
 CREATE TABLE Comments (
   	comment_id SERIAL PRIMARY KEY,
 	author VARCHAR(15),
-  	date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  	address VARCHAR(100) NOT NULL,
+  	date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  	address VARCHAR(100),
   	comment TEXT
 );
