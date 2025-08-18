dd4cf2ee5336f1ef455ba7e6bff6679df2302d34
zhenglena <zhenglena@gmail.com>
2025-08-18 15:43:57 -0500

    Simplify CommentsRequest body

--- src/main/java/com/lena/vigilande/dtos/CommentsRequest.java
+++ src/main/java/com/lena/vigilande/dtos/CommentsRequest.java
@@ -1,22 +1,12 @@
 package com.lena.vigilande.dtos;
 
 public class CommentsRequest {
-    private String address;
     private String comment;
     private String author;
 
-    public CommentsRequest(String address, String comment, String author) {
-        this.address = address;
+    public CommentsRequest(String comment, String author) {
         this.comment = comment;
         this.author = author;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getComment() {
