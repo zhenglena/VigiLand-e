b3bb961b0d7ea70fb514bebb26967eef59f5904e
zhenglena <zhenglena@gmail.com>
2025-08-18 20:32:27 -0500

    Add JdbcTemplate bean config

--- src/main/java/com/lena/vigilande/VigilandeApplication.java
+++ src/main/java/com/lena/vigilande/VigilandeApplication.java
@@ -2,12 +2,21 @@ package com.lena.vigilande;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class VigilandeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VigilandeApplication.class, args);
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
 	}
 
 }
