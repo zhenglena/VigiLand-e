49b2f4b1d765af82a054e308b7e1c6d05018dea5
zhenglena <zhenglena@gmail.com>
2025-08-19 21:34:58 -0500

    Add additional tests

--- src/test/java/com/lena/vigilande/parser/PathInputParserTest.java
+++ src/test/java/com/lena/vigilande/parser/PathInputParserTest.java
@@ -1,6 +1,6 @@
 package com.lena.vigilande.parser;
 
-import com.lena.vigilande.util.DateParser;
+import com.lena.vigilande.util.PathInputParser;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,13 +10,30 @@ import java.time.Month;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-public class DateParserTest {
+public class PathInputParserTest {
 
     @Test
-    public void dateParser_successful() {
+    public void parseDateFromCsv_successful() {
         String input = "08/08/2000";
-        LocalDate actualDate = DateParser.parseDateFromCsv(input);
+        LocalDate actualDate = PathInputParser.parseDateFromCsv(input);
 
         assertEquals(LocalDate.of(2000,Month.AUGUST,8), actualDate);
     }
+
+    @Test
+    public void parseDateFromUrl_successful() {
+        String input = "2000-08-08";
+        LocalDate actualDate = PathInputParser.parseDateFromUrl(input);
+
+        assertEquals(LocalDate.of(2000,Month.AUGUST,8), actualDate);
+    }
+
+    @Test
+    public void parseStringFromUrl_successful() {
+        String input = "123+W+GRACE+ST";
+        String actualString = PathInputParser.parseStringFromUrl(input);
+
+        assertEquals("123 W GRACE ST", actualString);
+    }
+
 }
